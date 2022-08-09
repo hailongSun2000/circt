2637b979486085537d1752da5dc73392d3c2c7b1
Zachary Yedidia <zyedidia@gmail.com>
2022-08-09 12:54:16 -0700

    Cleanup

--- lib/Dialect/Arc/InlineArcInputs.cpp
+++ lib/Dialect/Arc/InlineArcInputs.cpp
@@ -33,30 +33,33 @@ struct ConstArg {
 };
 
 void InlineArcInputsPass::runOnOperation() {
+  // For each arc, this map tracks which arguments are constant.
   DenseMap<StringRef, SmallVector<ConstArg>> arcConstArgs;
+  // All stateOps that use a particular arc.
   DenseMap<StringRef, SmallVector<StateOp>> arcUses;
 
   auto module = getOperation();
   for (auto moduleOp : llvm::make_early_inc_range(module.getOps<HWModuleOp>())) {
     for (auto op : moduleOp.getBody().getOps<StateOp>()) {
+      // Find all stateOps that use constant operands.
       SmallVector<ConstArg> constArgs;
-      auto ops = op.getOperands();
-      for (unsigned i = 1; i < op.getNumOperands(); i++) {
+      auto ops = op.operands();
+      for (unsigned i = 0; i < ops.size(); i++) {
         auto arg = ops[i];
-        if (auto c = dyn_cast_or_null<ConstantOp>(arg.getDefiningOp())) {
+        if (auto c = dyn_cast_or_null<ConstantOp>(arg.getDefiningOp()))
           constArgs.push_back({i, c});
-        }
       }
       auto arc = op.arc();
       arcUses[arc].push_back(op);
-      if (arcConstArgs.count(arc) == 0) {
-        arcConstArgs[arc] = constArgs;
-      } else if (arcConstArgs[arc] != constArgs) {
-        arcConstArgs.erase(arc);
-      }
+      if (arcConstArgs.count(arc) == 0)
+        arcConstArgs[arc] = constArgs; // make an entry for this arc
+      else if (arcConstArgs[arc] != constArgs)
+        arcConstArgs.erase(arc); // remove this arc if the constants are not equal to a previous use
     }
   }
 
+  // Now we go through all the defines and move the constant ops into the
+  // bodies and rewrite the function types.
   for (auto defOp : llvm::make_early_inc_range(module.getOps<DefineOp>())) {
     if (arcConstArgs.count(defOp.getName()) >= 1) {
       OpBuilder builder = OpBuilder::atBlockBegin(&defOp.bodyBlock());
@@ -64,20 +67,24 @@ void InlineArcInputsPass::runOnOperation() {
       DenseSet<unsigned> deletedArgs;
       SmallVector<unsigned> toDelete;
       for (auto arg : args) {
+        // Put the constant into the define's body.
         auto* newOp = arg.op->clone();
         builder.insert(newOp);
-        auto blockArg = defOp.getArgument(arg.i-1);
+        auto blockArg = defOp.getArgument(arg.i);
         blockArg.replaceAllUsesWith(newOp->getResult(0));
+        // Register the arg for deletion from the function signature.
         deletedArgs.insert(arg.i);
-        toDelete.push_back(arg.i-1);
+        toDelete.push_back(arg.i);
       }
       defOp.bodyBlock().eraseArguments(toDelete);
 
+      // Rewrite the FunctionType
       SmallVector<Type> inputTypes;
       SmallVector<Type> outputTypes;
       auto types = defOp.getArgumentTypes();
       for (unsigned i = 0; i < defOp.getNumArguments(); i++) {
-        if (deletedArgs.contains(i+1))
+        // Skip deleted inputs.
+        if (deletedArgs.contains(i))
           continue;
         inputTypes.push_back(types[i]);
       }
@@ -86,17 +93,19 @@ void InlineArcInputsPass::runOnOperation() {
 
       defOp.setType(builder.getFunctionType(inputTypes, outputTypes));
 
+      // Rewrite all stateOp arc uses to not pass in the constant anymore.
       auto uses = arcUses[defOp.getName()];
       for (auto stateOp : uses) {
         SmallVector<Value> inputs;
-        auto ops = stateOp.getOperands();
-        for (unsigned i = 1; i < stateOp.getNumOperands(); i++) {
+        auto ops = stateOp.operands();
+        for (unsigned i = 0; i < ops.size(); i++) {
           if (deletedArgs.contains(i))
             continue;
           inputs.push_back(ops[i]);
         }
 
         builder.setInsertionPoint(stateOp);
+        // Create a new stateOp with the reduced input vector.
         auto newStateOp = builder.create<StateOp>(stateOp->getLoc(), defOp, stateOp.clock(), stateOp.enable(), stateOp.latency(), inputs);
         stateOp.replaceAllUsesWith(newStateOp);
         stateOp.erase();
