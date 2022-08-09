521c83a0b903c2d5b14f307d6e31a75bb71dbfe8
Zachary Yedidia <zyedidia@gmail.com>
2022-08-09 11:44:34 -0700

    Inline inputs

--- lib/Dialect/Arc/InlineArcInputs.cpp
+++ lib/Dialect/Arc/InlineArcInputs.cpp
@@ -20,21 +20,89 @@ namespace {
 struct InlineArcInputsPass : public InlineArcInputsBase<InlineArcInputsPass> {
   void runOnOperation() override;
   void runOnModule();
-  HWModuleOp moduleOp;
 };
 } // namespace
 
-void InlineArcInputsPass::runOnOperation() {
-  auto module = getOperation();
-  for (auto op : llvm::make_early_inc_range(module.getOps<HWModuleOp>())) {
-    moduleOp = op;
-    runOnModule();
-  }
-}
+struct ConstArg {
+  unsigned i;
+  ConstantOp op;
 
-void InlineArcInputsPass::runOnModule() {
-  LLVM_DEBUG(llvm::dbgs() << "Inline arc inputs in `" << moduleOp.moduleName()
-                          << "`\n");
+  bool operator==(const ConstArg &a) const {
+    return a.i != i;
+  }
+};
+
+void InlineArcInputsPass::runOnOperation() {
+  DenseMap<StringRef, SmallVector<ConstArg>> arcConstArgs;
+  DenseMap<StringRef, SmallVector<StateOp>> arcUses;
+
+  auto module = getOperation();
+  for (auto moduleOp : llvm::make_early_inc_range(module.getOps<HWModuleOp>())) {
+    for (auto op : moduleOp.getBody().getOps<StateOp>()) {
+      SmallVector<ConstArg> constArgs;
+      auto ops = op.getOperands();
+      for (unsigned i = 1; i < op.getNumOperands(); i++) {
+        auto arg = ops[i];
+        if (auto c = dyn_cast_or_null<ConstantOp>(arg.getDefiningOp())) {
+          constArgs.push_back({i, c});
+        }
+      }
+      auto arc = op.arc();
+      arcUses[arc].push_back(op);
+      if (arcConstArgs.count(arc) == 0) {
+        arcConstArgs[arc] = constArgs;
+      } else if (arcConstArgs[arc] != constArgs) {
+        arcConstArgs.erase(arc);
+      }
+    }
+  }
+
+  for (auto defOp : llvm::make_early_inc_range(module.getOps<DefineOp>())) {
+    if (arcConstArgs.count(defOp.getName()) >= 1) {
+      OpBuilder builder = OpBuilder::atBlockBegin(&defOp.bodyBlock());
+      auto args = arcConstArgs[defOp.getName()];
+      DenseSet<unsigned> deletedArgs;
+      SmallVector<unsigned> toDelete;
+      for (auto arg : args) {
+        auto* newOp = arg.op->clone();
+        builder.insert(newOp);
+        auto blockArg = defOp.getArgument(arg.i-1);
+        blockArg.replaceAllUsesWith(newOp->getResult(0));
+        deletedArgs.insert(arg.i);
+        toDelete.push_back(arg.i-1);
+      }
+      defOp.bodyBlock().eraseArguments(toDelete);
+
+      SmallVector<Type> inputTypes;
+      SmallVector<Type> outputTypes;
+      auto types = defOp.getArgumentTypes();
+      for (unsigned i = 0; i < defOp.getNumArguments(); i++) {
+        if (deletedArgs.contains(i+1))
+          continue;
+        inputTypes.push_back(types[i]);
+      }
+      for (auto type : defOp.getResultTypes())
+        outputTypes.push_back(type);
+
+      defOp.setType(builder.getFunctionType(inputTypes, outputTypes));
+
+      auto uses = arcUses[defOp.getName()];
+      for (auto stateOp : uses) {
+        SmallVector<Value> inputs;
+        auto ops = stateOp.getOperands();
+        for (unsigned i = 1; i < stateOp.getNumOperands(); i++) {
+          if (deletedArgs.contains(i))
+            continue;
+          inputs.push_back(ops[i]);
+        }
+
+        builder.setInsertionPoint(stateOp);
+        auto newStateOp = builder.create<StateOp>(stateOp->getLoc(), defOp, stateOp.clock(), stateOp.enable(), stateOp.latency(), inputs);
+        stateOp.replaceAllUsesWith(newStateOp);
+        stateOp.erase();
+      }
+    }
+  }
 }
 
 namespace circt {
