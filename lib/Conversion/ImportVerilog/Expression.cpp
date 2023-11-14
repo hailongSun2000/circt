a2092b9fa9d2f45e08b11c2febdcb9bf0e62fb7b
hailong <hailong.sun@terapines.com>
2023-11-14 16:33:39 +0800

    [ImportVerilog] Implement the body of the visitBinaryOp func for choosing which op will be used.

--- lib/Conversion/ImportVerilog/Expression.cpp
+++ lib/Conversion/ImportVerilog/Expression.cpp
@@ -42,6 +42,108 @@ Value Context::visitNamedValue(
   // return nullptr;
 }
 
+// Detail processing about binary expression.
+Value Context::visitBinaryOp(const slang::ast::BinaryExpression *binaryExpr,
+                             const slang::ast::Type &type) {
+  auto loc = convertLocation(binaryExpr->sourceRange.start());
+  Value lhs = visitExpression(&binaryExpr->left(), type);
+  if (!lhs)
+    return nullptr;
+  Value rhs = visitExpression(&binaryExpr->right(), type);
+  if (!rhs)
+    return nullptr;
+
+  switch (binaryExpr->op) {
+  case slang::ast::BinaryOperator::Add:
+    mlir::emitError(loc, "unsupported binary operator : add");
+    return nullptr;
+  case slang::ast::BinaryOperator::Subtract:
+    mlir::emitError(loc, "unsupported binary operator : subtract");
+    return nullptr;
+  case slang::ast::BinaryOperator::Multiply:
+    mlir::emitError(loc, "unsupported binary operator : multiply");
+    return nullptr;
+  case slang::ast::BinaryOperator::Divide:
+    mlir::emitError(loc, "unsupported binary operator : divide");
+    return nullptr;
+  case slang::ast::BinaryOperator::Mod:
+    mlir::emitError(loc, "unsupported binary operator : mod");
+    return nullptr;
+  case slang::ast::BinaryOperator::BinaryAnd:
+    mlir::emitError(loc, "unsupported binary operator : binary and");
+    return nullptr;
+  case slang::ast::BinaryOperator::BinaryOr:
+    mlir::emitError(loc, "unsupported binary operator : binary or");
+    return nullptr;
+  case slang::ast::BinaryOperator::BinaryXor:
+    mlir::emitError(loc, "unsupported binary operator : binary xor");
+    return nullptr;
+  case slang::ast::BinaryOperator::BinaryXnor:
+    mlir::emitError(loc, "unsupported binary operator : binary xnor");
+    return nullptr;
+  case slang::ast::BinaryOperator::Equality:
+    return rootBuilder.create<moore::EqualityOp>(loc, lhs, rhs);
+  case slang::ast::BinaryOperator::Inequality:
+    return rootBuilder.create<moore::InEqualityOp>(loc, lhs, rhs);
+  case slang::ast::BinaryOperator::CaseEquality:
+    return rootBuilder.create<moore::EqualityOp>(loc, lhs, rhs,
+                                                 rootBuilder.getUnitAttr());
+  case slang::ast::BinaryOperator::CaseInequality:
+    return rootBuilder.create<moore::InEqualityOp>(loc, lhs, rhs,
+                                                   rootBuilder.getUnitAttr());
+  case slang::ast::BinaryOperator::GreaterThanEqual:
+    mlir::emitError(loc, "unsupported binary operator : greater than equal");
+    return nullptr;
+  case slang::ast::BinaryOperator::GreaterThan:
+    mlir::emitError(loc, "unsupported binary operator : greater than");
+    return nullptr;
+  case slang::ast::BinaryOperator::LessThanEqual:
+    mlir::emitError(loc, "unsupported binary operator : less than equal");
+    return nullptr;
+  case slang::ast::BinaryOperator::LessThan:
+    mlir::emitError(loc, "unsupported binary operator : less than");
+    return nullptr;
+  case slang::ast::BinaryOperator::WildcardEquality:
+    mlir::emitError(loc, "unsupported binary operator : wildcard equality");
+    return nullptr;
+  case slang::ast::BinaryOperator::WildcardInequality:
+    mlir::emitError(loc, "unsupported binary operator : wildcard inequality");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalAnd:
+    mlir::emitError(loc, "unsupported binary operator : logical and");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalOr:
+    mlir::emitError(loc, "unsupported binary operator : logical or");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalImplication:
+    mlir::emitError(loc, "unsupported binary operator : logical implication");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalEquivalence:
+    mlir::emitError(loc, "unsupported binary operator : logical equivalence");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalShiftLeft:
+    mlir::emitError(loc, "unsupported binary operator : logical shift left");
+    return nullptr;
+  case slang::ast::BinaryOperator::LogicalShiftRight:
+    mlir::emitError(loc, "unsupported binary operator : logical shift right");
+    return nullptr;
+  case slang::ast::BinaryOperator::ArithmeticShiftLeft:
+    mlir::emitError(loc, "unsupported binary operator : arithmetic shift left");
+    return nullptr;
+  case slang::ast::BinaryOperator::ArithmeticShiftRight:
+    mlir::emitError(loc,
+                    "unsupported binary operator : arithmetic shift right");
+    return nullptr;
+  case slang::ast::BinaryOperator::Power:
+    mlir::emitError(loc, "unsupported binary operator : power");
+    return nullptr;
+  default:
+    mlir::emitError(loc, "unsupported binary operator");
+    return nullptr;
+  }
+  return nullptr;
+}
+
 // Detail processing about assignment.
 Value Context::visitAssignmentExpr(
     const slang::ast::AssignmentExpression *assignmentExpr,
@@ -58,8 +160,16 @@ Value Context::visitAssignmentExpr(
   //   mlir::emitError(loc, "unsupported assignment of kind like a = b");
   //   return nullptr;
   // }
+  // if (assignmentExpr->isNonBlocking())
+  //   rootBuilder.create<moore::PAssignOp>(loc, lhs, rhs);
+  // else {
+  //   if (assignmentExpr->syntax->parent->kind ==
+  //       slang::syntax::SyntaxKind::ContinuousAssign)
+  //     rootBuilder.create<moore::AssignOp>(loc, lhs, rhs);
+  //   else
   rootBuilder.create<moore::BPAssignOp>(loc, lhs, rhs);
-  return nullptr;
+  // }
+  return lhs;
 }
 
 // Detail processing about conversion
@@ -76,8 +186,8 @@ Value Context::visitConversion(
         &conversionExpr->operand().as<slang::ast::NamedValueExpression>(),
         type);
   case slang::ast::ExpressionKind::BinaryOp:
-    mlir::emitError(loc, "unsupported conversion expression: binary operator");
-    return nullptr;
+    return visitBinaryOp(
+        &conversionExpr->operand().as<slang::ast::BinaryExpression>(), type);
   case slang::ast::ExpressionKind::ConditionalOp:
     mlir::emitError(loc,
                     "unsupported conversion expression: conditional operator");
@@ -110,6 +220,8 @@ Value Context::visitExpression(const slang::ast::Expression *expression,
   case slang::ast::ExpressionKind::NamedValue:
     return visitNamedValue(&expression->as<slang::ast::NamedValueExpression>(),
                            type);
+  case slang::ast::ExpressionKind::BinaryOp:
+    return visitBinaryOp(&expression->as<slang::ast::BinaryExpression>(), type);
   case slang::ast::ExpressionKind::Assignment:
     return visitAssignmentExpr(
         &expression->as<slang::ast::AssignmentExpression>(), type);
